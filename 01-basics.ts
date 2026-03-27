04046d1d0c1daaf58708dd407c1be0f877db195c
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2026-03-27 16:31:41 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,11 @@
-// console.log("Hello World!")
+console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Vhaugnn";
+let userAge = 24;
 
 // ...
 
-userAge = "31"
+userAge = 24
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
