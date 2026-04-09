165099db7f153bbb7b5130a3875384d86feb7af5
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2026-04-09 18:08:44 +0800

    Done playing and updating the 06-alternative-to-enum.ts file

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -1,11 +1,13 @@
 (() => {
     // Literal Types (Alternative to Enum)
-    let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
+    let userRole: 'admin' | 'guest' | 'teacher' | 'student' = 'admin';
 
-    userRole = 'guess'
-    // anotherUserRole = 'superadmin'
+    userRole = 'student';
+    // userRole = 'superadmin';
 
-    function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+    function access(role: 'admin' | 'guest' | 'teacher' | 'student') {
+        console.log("Accessing role:", role);
     }
-})
+
+    access(userRole);
+})();
